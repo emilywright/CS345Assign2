a428291dd8d9fc63c9e82d309c26081f9a2b94a8
Silverfin13 <shahobmmousavi@gmail.com>
2017-11-02 17:32:00 -0700

    inital commit

---
+++ Deadwood.java
@@ -0,0 +1,40 @@
+/**
+ * Created by mousavs on 11/2/17.
+ */
+public class Deadwood {
+    public static void main(String[] args) {
+
+    }
+
+    public static int gameSetup(){
+        int numberPlayers;
+        //startGame() numberPlayers;
+
+    }
+
+    public static int gameManager() {
+
+    }
+
+
+    public static int player() {
+
+
+    }
+    public static int castingOffice() {
+
+    }
+
+    public static int scenes() {
+
+
+    }
+    public static int dice() {
+
+    }
+
+
+
+
+
+}
