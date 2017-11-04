d8c11701fb86051a83eaa9fafb0ad73f1f4223d0
jindodog89 <jindodog89@gmail.com>
2017-11-04 14:31:51 -0700

    Create Rehearse.java

---
+++ Rehearse.java
@@ -0,0 +1,21 @@
+public class Rehearse {
+    
+    int sceneNumber = 0;
+    boolean rehearseMarker = true;
+    int playerNumber = 0;
+    int diceValue = 0;
+    int reheaseValue = 0;
+    
+    public Rehearse(){}
+    
+    public int actingRoleValue(){}
+    
+    public void increaseRehearseValue(){
+        return rehearseValue + 1;
+    }
+    
+    public void forceActing(){
+    
+    }
+    
+}
