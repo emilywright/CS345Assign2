99ab71b76c750ae425a7f74898dcb68b2c8041b1
jindodog89 <jindodog89@gmail.com>
2017-11-09 19:02:00 -0800

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -2,16 +2,16 @@ import java.lang.*;
 
 public class Dice {
 
+    //die1 is rankdie
+    //die2 is a regular die to get dice value along with die1.
     private static int die1 = 0;
     private static int die2 = 0;
     private static int diceValue = 0;
-    private static int whichPlayer = 0.0;
+    private static int whichPlayer = 0;
     private static int rankDie = 0;
-    //rankDie = die1
 
-
-    public Dice() {
-        rollDice();
+    public Dice(int player) {
+        whichPlayer = player;
     }
 
     public void rollDice() {
@@ -19,7 +19,11 @@ public class Dice {
         die2 = (int)(Math.random()*6) + 1;
         diceValue = die1+die2;
     }
-
+    
+    public int getDiceValue(){
+        return diceValue;
+    }
+    
     public int getRankDie(){
         return die1;
     }
